a51c6efdf2e703779b3bfa451753f225afc76f76
RohendoJ <rohendoj3134.github@gmail.com>
2024-05-01 09:43:10 +0700

    improve: register validation

--- mobile/lib/screens/auth/register.dart
+++ mobile/lib/screens/auth/register.dart
@@ -4,29 +4,58 @@ import 'package:tour_app/screens/auth/main.dart';
 import 'package:tour_app/services/services_signup.dart';
 import 'package:tour_app/model/model_signup.dart';
 
-class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({Key? key});
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController username = TextEditingController();
+  final TextEditingController fullname = TextEditingController();
+  final TextEditingController password = TextEditingController();
+  final TextEditingController cpassword = TextEditingController();
+
+  bool isLoading = false;
+  bool isButtonDisabled = true;
+
+  final SignUpService _signUpService = SignUpService();
+
+  @override
+  void initState() {
+    super.initState();
+    username.addListener(validateInput);
+    fullname.addListener(validateInput);
+    password.addListener(validateInput);
+    cpassword.addListener(validateInput);
+  }
+
+  void validateInput() {
+    setState(() {
+      isButtonDisabled = !(username.text.isNotEmpty &&
+          fullname.text.isNotEmpty &&
+          password.text.isNotEmpty &&
+          cpassword.text.isNotEmpty &&
+          password.text == cpassword.text);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController username = TextEditingController();
-    final TextEditingController fullname = TextEditingController();
-    final TextEditingController password = TextEditingController();
-    final TextEditingController cpassword = TextEditingController();
-
-    final SignUpService _signUpService = SignUpService();
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Daftar Akun'),
         leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const AuthScreen()),
-              );
-            }),
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const AuthScreen()),
+            );
+          },
+        ),
       ),
       backgroundColor: Colors.white,
       body: Center(
@@ -36,9 +65,10 @@ class RegisterScreen extends StatelessWidget {
             const Text(
               'Daftar Tour App',
               style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.teal,
-                  fontWeight: FontWeight.bold),
+                fontSize: 30,
+                color: Colors.teal,
+                fontWeight: FontWeight.bold,
+              ),
             ),
             const SizedBox(height: 30),
             FractionallySizedBox(
@@ -90,43 +120,78 @@ class RegisterScreen extends StatelessWidget {
             FractionallySizedBox(
               widthFactor: 0.7,
               child: ElevatedButton(
-                onPressed: () async {
-                  if (password.text == cpassword.text) {
-                    ModelSignUp? signUpResponse =
-                        await _signUpService.signUpAccount(
-                      username.text,
-                      fullname.text,
-                      password.text,
-                    );
+                onPressed: isButtonDisabled || isLoading
+                    ? null
+                    : () async {
+                        setState(() {
+                          isLoading = true;
+                        });
 
-                    if (signUpResponse != null) {
-                      print('Berhasil mendaftar: ${signUpResponse.message}');
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => LoginScreen(),
-                        ),
-                      );
-                    } else {
-                      print('Gagal mendaftar');
-                    }
-                  } else {
-                    print('Password dan konfirmasi password tidak cocok');
-                  }
-                },
+                        ModelSignUp? signUpResponse =
+                            await _signUpService.signUpAccount(
+                          username.text,
+                          fullname.text,
+                          password.text,
+                        );
+
+                        if (signUpResponse != null) {
+                          Navigator.pushReplacement(
+                            // ignore: use_build_context_synchronously
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const LoginScreen(),
+                            ),
+                          );
+                        } else {
+                          setState(() {
+                            isLoading = false;
+                          });
+
+                          showDialog(
+                              // ignore: use_build_context_synchronously
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                    title: const Text('Error'),
+                                    content: const Text(
+                                      'Terjadi kesalahan saat melakukan daftar',
+                                    ),
+                                    actions: [
+                                      TextButton(
+                                          child: const Text('OK'),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          })
+                                    ]);
+                              });
+                        }
+                      },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.tealAccent[700],
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 12),
-                  child: Text(
-                    'Daftar',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
+                child: isLoading
+                    ? Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 40, vertical: 12),
+                        child: SizedBox(
+                            height: 20,
+                            width: 20,
+                            child: CircularProgressIndicator(
+                              strokeWidth: 3,
+                              color: Colors.tealAccent[300],
+                            )),
+                      )
+                    : const Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                        child: Text(
+                          'Daftar',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
               ),
             ),
             const SizedBox(height: 10),
