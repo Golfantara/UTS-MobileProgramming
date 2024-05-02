3216e395b44995a142597c5370edc2eecf4a0752
Golfantara <golfantara7@gmail.com>
2024-05-02 07:40:01 +0700

    bugfix: fix data tours

--- backend/features/tours/dtos/response.go
+++ backend/features/tours/dtos/response.go
@@ -6,7 +6,9 @@ type ResTours struct {
 	ID			int		`json:"id" form:"id"`
 	UserID		int		`json:"user_id" form:"user_id"`
 	Name       string `json:"name" form:"name"`
+	ProvinsiID string `json:"provinsi_id" form:"provinsi_id"`
 	Provinsi   string `json:"provinsi" form:"provinsi"`
+	KabkotID string `json:"kabkot_id" form:"kabkot_id"`
 	Kabkot     string `json:"kabkot" form:"kabkot"`
 	Latitude   string `json:"latitude" form:"latitude"`
 	Longtitude string `json:"longtitude" form:"longtitude"`
--- backend/features/tours/entities.go
+++ backend/features/tours/entities.go
@@ -13,7 +13,9 @@ type Tours struct {
 	UserID      int     `gorm:"type:int(11)"`
 	Name		string  `gorm:"type:string"`
 	Provinsi 	string	`gorm:"type:string"`
+	ProvinsiID  string  `gorm:"type:string"`
 	Kabkot		string	`gorm:"type:string"`
+	KabkotID  string  `gorm:"type:string"`
 	Latitude    string	`gorm:"type:string"`
 	Longtitude  string	`gorm:"type:string"`
 	Images		string	`gorm:"type:text"`
