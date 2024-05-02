260feea34d1d293c2e105778bfb1fd58e0f3163f
RohendoJ <rohendoj3134.github@gmail.com>
2024-05-02 07:26:04 +0700

    validation: create tour

--- mobile/lib/screens/tours/create_tour.dart
+++ mobile/lib/screens/tours/create_tour.dart
@@ -29,6 +29,7 @@ class _CreateTourScreenState extends State<CreateTourScreen> {
   List<Map<String, String>> provincesData = [];
   List<Map<String, String>> regenciesData = [];
   String? accessToken;
+  bool isValid = false;
 
   @override
   void initState() {
@@ -44,16 +45,35 @@ class _CreateTourScreenState extends State<CreateTourScreen> {
     });
   }
 
-  Future<void> _createTour(
-      String name,
-      String provinsi,
-      String kabkot,
-      String latitude,
-      String longtitude,
-      dynamic images,
-      String accessToken) async {
+  void _validateInputs() {
+    setState(() {
+      isValid = name.text.isNotEmpty &&
+          latitude.text.isNotEmpty &&
+          longtitude.text.isNotEmpty &&
+          province != null &&
+          regency != null &&
+          _image != null;
+    });
+  }
+
+  void _createTour(
+    String name,
+    String provinsi,
+    String kabkot,
+    String latitude,
+    String longtitude,
+    dynamic images,
+    String accessToken,
+  ) async {
     final data = await _toursServices.createTour(
-        name, provinsi, kabkot, latitude, longtitude, images, accessToken);
+      name,
+      provinsi,
+      kabkot,
+      latitude,
+      longtitude,
+      images,
+      accessToken,
+    );
 
     if (data != null) {
       Navigator.push(
@@ -89,7 +109,7 @@ class _CreateTourScreenState extends State<CreateTourScreen> {
     }
   }
 
-  Future getImageFromGallery() async {
+  Future<void> getImageFromGallery() async {
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
 
     setState(() {
@@ -97,9 +117,11 @@ class _CreateTourScreenState extends State<CreateTourScreen> {
         _image = File(pickedFile.path);
       }
     });
+
+    _validateInputs();
   }
 
-  Future getImageFromCamera() async {
+  Future<void> getImageFromCamera() async {
     final pickedFile = await picker.pickImage(source: ImageSource.camera);
 
     setState(() {
@@ -107,6 +129,8 @@ class _CreateTourScreenState extends State<CreateTourScreen> {
         _image = File(pickedFile.path);
       }
     });
+
+    _validateInputs();
   }
 
   Future<void> showOptions(BuildContext context) async {
@@ -120,7 +144,6 @@ class _CreateTourScreenState extends State<CreateTourScreen> {
               title: const Text('Gallery'),
               onTap: () {
                 Navigator.of(context).pop();
-
                 getImageFromGallery();
               },
             ),
@@ -128,7 +151,6 @@ class _CreateTourScreenState extends State<CreateTourScreen> {
               title: const Text('Camera'),
               onTap: () {
                 Navigator.of(context).pop();
-
                 getImageFromCamera();
               },
             ),
@@ -141,170 +163,178 @@ class _CreateTourScreenState extends State<CreateTourScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: const Text('Buat Tour', style: TextStyle(color: Colors.teal)),
-        ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              children: [
-                const SizedBox(height: 20),
-                FractionallySizedBox(
-                  widthFactor: 0.7,
-                  child: TextField(
-                    controller: name,
-                    decoration: const InputDecoration(
-                      labelText: 'Masukan nama tour',
-                      border: OutlineInputBorder(),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Buat Tour', style: TextStyle(color: Colors.teal)),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              FractionallySizedBox(
+                widthFactor: 0.7,
+                child: TextField(
+                  controller: name,
+                  onChanged: (_) => _validateInputs(),
+                  decoration: const InputDecoration(
+                    labelText: 'Masukan nama tour',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              FractionallySizedBox(
+                widthFactor: 0.7,
+                child: DropdownButton<String>(
+                  isExpanded: true,
+                  value: province,
+                  hint: const Text('Pilih Provinsi'),
+                  elevation: 16,
+                  underline: Container(
+                    height: 2,
+                    color: Colors.teal,
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      province = newValue!;
+                    });
+                    setState(() {
+                      regency = null;
+                    });
+                    _fetchRegencies(newValue!);
+                    _validateInputs();
+                  },
+                  items: provincesData.map((Map<String, String> province) {
+                    return DropdownMenuItem(
+                      value: province['id'],
+                      child: Text(province['name']!),
+                    );
+                  }).toList(),
+                ),
+              ),
+              const SizedBox(height: 10),
+              FractionallySizedBox(
+                widthFactor: 0.7,
+                child: DropdownButton<String>(
+                  isExpanded: true,
+                  value: regency,
+                  hint: const Text('Pilih Kota/Kabupaten'),
+                  elevation: 16,
+                  underline: Container(
+                    height: 2,
+                    color: Colors.teal,
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      regency = newValue!;
+                    });
+                    _validateInputs();
+                  },
+                  items: regenciesData.map((Map<String, String> regency) {
+                    return DropdownMenuItem(
+                      value: regency['id'],
+                      child: Text(regency['name']!),
+                    );
+                  }).toList(),
+                ),
+              ),
+              const SizedBox(height: 10),
+              FractionallySizedBox(
+                widthFactor: 0.7,
+                child: TextField(
+                  controller: latitude,
+                  onChanged: (_) => _validateInputs(),
+                  decoration: const InputDecoration(
+                    labelText: 'Masukan Latitude',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              FractionallySizedBox(
+                widthFactor: 0.7,
+                child: TextField(
+                  controller: longtitude,
+                  onChanged: (_) => _validateInputs(),
+                  decoration: const InputDecoration(
+                    labelText: 'Masukan longtitude',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              FractionallySizedBox(
+                widthFactor: 0.7,
+                child: OutlinedButton(
+                  onPressed: () {
+                    showOptions(context);
+                  },
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    side: const BorderSide(color: Colors.teal),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 36, vertical: 12),
+                    child: Text(
+                      'Ambil gambar',
+                      style: TextStyle(color: Colors.teal),
                     ),
                   ),
                 ),
-                const SizedBox(height: 10),
-                FractionallySizedBox(
-                  widthFactor: 0.7,
-                  child: DropdownButton<String>(
-                    isExpanded: true,
-                    value: province,
-                    hint: const Text('Pilih Provinsi'),
-                    elevation: 16,
-                    underline: Container(
-                      height: 2,
-                      color: Colors.teal,
-                    ),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        province = newValue!;
-                      });
-                      setState(() {
-                        regency = null;
-                      });
-                      _fetchRegencies(newValue!);
-                    },
-                    items: provincesData.map((Map<String, String> province) {
-                      return DropdownMenuItem(
-                        value: province['id'],
-                        child: Text(province['name']!),
-                      );
-                    }).toList(),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                FractionallySizedBox(
-                  widthFactor: 0.7,
-                  child: DropdownButton<String>(
-                      isExpanded: true,
-                      value: regency,
-                      hint: const Text('Pilih Kota/Kabupaten'),
-                      elevation: 16,
-                      underline: Container(
-                        height: 2,
-                        color: Colors.teal,
+              ),
+              const SizedBox(height: 10),
+              FractionallySizedBox(
+                widthFactor: 0.7,
+                child: _image == null
+                    ? Image.network(
+                        "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png",
+                        height: 200,
+                      )
+                    : Image.file(
+                        _image!,
+                        height: 200,
                       ),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          regency = newValue!;
-                        });
-                      },
-                      items: regenciesData.map((Map<String, String> regency) {
-                        return DropdownMenuItem(
-                          value: regency['id'],
-                          child: Text(regency['name']!),
-                        );
-                      }).toList()),
-                ),
-                FractionallySizedBox(
-                  widthFactor: 0.7,
-                  child: TextField(
-                    controller: latitude,
-                    decoration: const InputDecoration(
-                      labelText: 'Masukan Latitude',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                FractionallySizedBox(
-                  widthFactor: 0.7,
-                  child: TextField(
-                    controller: longtitude,
-                    decoration: const InputDecoration(
-                      labelText: 'Masukan longtitude',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                FractionallySizedBox(
-                  widthFactor: 0.7,
-                  child: OutlinedButton(
-                    onPressed: () {
-                      showOptions(context);
-                    },
-                    style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(10), // Rounded edges
-                      ),
-                      side: const BorderSide(color: Colors.teal), // Teal border
-                    ),
-                    child: const Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 36, vertical: 12),
-                      child: Text(
-                        'Ambil gambar',
-                        style: TextStyle(color: Colors.teal),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                FractionallySizedBox(
-                  widthFactor: 0.7,
-                  child: _image == null
-                      ? Image.network(
-                          "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png",
-                          height: 200,
-                        )
-                      : Image.file(
-                          _image!,
-                          height: 200,
-                        ),
-                ),
-                const SizedBox(height: 15),
-                FractionallySizedBox(
-                    widthFactor: 0.7,
-                    child: ElevatedButton(
-                        onPressed: () async {
+              ),
+              const SizedBox(height: 15),
+              FractionallySizedBox(
+                widthFactor: 0.7,
+                child: ElevatedButton(
+                  onPressed: isValid
+                      ? () async {
                           _createTour(
-                            name.text, // Gunakan name.text untuk mengambil nilai dari TextEditingController
+                            name.text,
                             province!,
                             regency!,
-                            latitude
-                                .text, // Gunakan latitude.text untuk mengambil nilai dari TextEditingController
-                            longtitude
-                                .text, // Gunakan longtitude.text untuk mengambil nilai dari TextEditingController
+                            latitude.text,
+                            longtitude.text,
                             _image!,
                             accessToken!,
                           );
-                        },
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.tealAccent[700],
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            )),
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 40, vertical: 12),
-                          child: Text(
-                            'Simpan data',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ))),
-                const SizedBox(height: 20)
-              ],
-            ),
+                        }
+                      : null,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.tealAccent[700],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                    child: Text(
+                      'Simpan data',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20)
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
