a8cd7eebbae1fe3b0ea37b7692300aea0124030b
Golfantara <golfantara7@gmail.com>
2024-05-02 07:41:21 +0700

    fix: fix dtos tours

--- backend/features/tours/dtos/request.go
+++ backend/features/tours/dtos/request.go
@@ -2,7 +2,9 @@ package dtos
 
 type InputTours struct {
 	Name       string `json:"name" form:"name"`
+	ProvinsiID string `json:"provinsi_id" form:"provinsi_id"`
 	Provinsi   string `json:"provinsi" form:"provinsi"`
+	KabkotID   string `json:"kabkot_id" form:"kabkot_id"`
 	Kabkot     string `json:"kabkot" form:"kabkot"`
 	Latitude   string `json:"latitude" form:"latitude"`
 	Longtitude string `json:"longtitude" form:"longtitude"`
